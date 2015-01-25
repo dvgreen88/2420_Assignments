ac7bb058f61edf24b9524c2d70471b5dcd52a22b
David Weber <davidlweber@gmail.com>
2015-01-25 15:13:30 -0700

    Initial upload

---
+++ src/percolation/Percolation.java
@@ -0,0 +1,44 @@
+/**
+ * @author in order of appearance: David Weber, Vance Green
+ * @Date created: 1/24/2015
+ * @Date last modified:
+ */
+package percolation;
+
+public class Percolation {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+		
+	public Percolation(int n) {
+
+	}
+
+	
+	public boolean isFull(int row, int col) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isOpen(int row, int col) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String numberOfOpenSites() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean percolates() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void open(int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
