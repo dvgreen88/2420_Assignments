845bec2e7ea602c00354839e27cc051efb6f05fe
David Weber <davidlweber@gmail.com>
2015-01-26 23:46:55 -0700

    initial upload

---
+++ tests/percolation/PercolationTest.java
@@ -0,0 +1,39 @@
+package percolation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PercolationTest {
+
+	@Test
+	public void testPercolation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsFull() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsOpen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNumberOfOpenSites() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPercolates() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOpen() {
+		fail("Not yet implemented");
+	}
+
+}
